69ee27fc7af8280cabdb790ff669a5bfb9275373
jorgevs <jevzsz@gmail.com>
2017-06-04 19:28:55 -0400

    Adding ShapeRendererSample

--- core/src/common/SampleInfos.java
+++ core/src/common/SampleInfos.java
@@ -17,7 +17,8 @@ public class SampleInfos {
             GdxReflectionSample.SAMPLE_INFO,
             OrthographicCameraSample.SAMPLE_INFO,
             ViewportSample.SAMPLE_INFO,
-            SpriteBatchSample.SAMPLE_INFO
+            SpriteBatchSample.SAMPLE_INFO,
+            ShapeRendererSample.SAMPLE_INFO
     );
 
     private SampleInfos() {
